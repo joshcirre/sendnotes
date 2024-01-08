0c8aae2323ca3fa5b2f9b6a301a23f96b0815167
Josh Cirre <joshcirre@gmail.com>
2024-01-08 13:08:02 -0700

    add margin for fly deploy

--- resources/views/notes/view.blade.php
+++ resources/views/notes/view.blade.php
@@ -5,7 +5,7 @@
         </h2>
     </div>
     <p class="mt-4 mb-12">{{ $note->body }}</p>
-    <div class="flex items-center justify-end mt-12 space-x-2">
+    <div class="flex items-center justify-end mt-12 mb-2 space-x-2 ">
         <h3 class="mr-2 text-sm">Sent from {{ $user->name }}</h3>
         <livewire:heartreact :note="$note" />
     </div>
